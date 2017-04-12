aff377980aa75a4574d8302e237e4e7319c0dfe2
Andrew Jarombek <ajarombek95@gmail.com>
2017-04-12 15:34:06 -0400

    First Commit

---
+++ facts.pl
@@ -0,0 +1,17 @@
+% Author: Andrew Jarombek
+% Date: 4/10/2017
+% Contains facts about mastermind board game
+
+% The user placed pegs
+peg(blue).
+peg(green).
+peg(orange).
+peg(purple).
+peg(red).
+peg(yellow).
+
+% The gamemasters feedback pegs
+feedbackpeg(black).
+feedbackpeg(white).
+
+solution(blue, red, yellow, green).
---
+++ rules.pl
@@ -0,0 +1,5 @@
+% Author: Andrew Jarombek
+% Date: 4/10/2017
+% Contains rules about mastermind board game
+
+win(W, X, Y, Z) :- solution(W, X, Y, Z)
